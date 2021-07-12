fc08b8a2f13794cb934f334316a73821d753ba8b
JoseCalvi1 <josecalvilloolmedo@gmail.com>
2021-07-12 17:02:34 +0200

    Agregar y configurar medium editor

--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/medium-editor/5.23.3/css/medium-editor.min.css" integrity="sha512-zYqhQjtcNMt8/h4RJallhYRev/et7+k/HDyry20li5fWSJYSExP9O07Ung28MUuXDneIFg0f2/U3HJZWsTNAiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('navegacion')
     @include('ui.adminnav')
 @endsection
@@ -19,7 +23,7 @@
         <div class="mb-5">
             <label for="categoria" class="block text-gray-700 text-sm mb-2">Categoría: </label>
 
-            <select id="categoria" name="categoria" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 bg-gray-100">
+            <select id="categoria" name="categoria" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 white">
                 <option disabled selected>- Selecciona -</option>
                 @foreach ($categorias as $categoria)
                     <option value="{{ $categoria->id }}">
@@ -32,7 +36,7 @@
         <div class="mb-5">
             <label for="experiencia" class="block text-gray-700 text-sm mb-2">Experiencia: </label>
 
-            <select id="experiencia" name="experiencia" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 bg-gray-100">
+            <select id="experiencia" name="experiencia" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 white">
                 <option disabled selected>- Selecciona -</option>
                 @foreach ($experiencias as $experiencia)
                     <option value="{{ $experiencia->id }}">
@@ -45,7 +49,7 @@
         <div class="mb-5">
             <label for="ubicacion" class="block text-gray-700 text-sm mb-2">Ubicación: </label>
 
-            <select id="ubicacion" name="ubicacion" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 bg-gray-100">
+            <select id="ubicacion" name="ubicacion" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 white">
                 <option disabled selected>- Selecciona -</option>
                 @foreach ($ubicaciones as $ubicacion)
                     <option value="{{ $ubicacion->id }}">
@@ -58,7 +62,7 @@
         <div class="mb-5">
             <label for="salario" class="block text-gray-700 text-sm mb-2">Salario: </label>
 
-            <select id="salario" name="salario" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 bg-gray-100">
+            <select id="salario" name="salario" class="block apperiance-none w-full border border-gray-700 rounded leading-tight focus:outline-nonefocus:bg-white focus:border-gray-500 p-3 white">
                 <option disabled selected>- Selecciona -</option>
                 @foreach ($salarios as $salario)
                     <option value="{{ $salario->id }}">
@@ -68,7 +72,36 @@
             </select>
         </div>
 
+        <div class="mb-5">
+            <label for="descripcion" class="block text-gray-700 text-sm mb-2">Descripción del puesto: </label>
+            <div class="editable p-3 bg-white rounded form-input w-full text-gray-700"></div>
+            <input type="hidden" name="descripcion" id="descripcion">
+        </div>
+
         <button type="submit" class="bg-blue-500 w-full hover:bg-blue-600 text-gray-100 font-bold p-3 focus:outline focus:shadow-outline uppercase">Publicar vacante</button>
     </form>
 
+@endsection
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/medium-editor/5.23.3/js/medium-editor.min.js" integrity="sha512-5D/0tAVbq1D3ZAzbxOnvpLt7Jl/n8m/YGASscHTNYsBvTcJnrYNiDIJm6We0RPJCpFJWowOPNz9ZJx7Ei+yFiA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const editor = new MediumEditor('.editable', {
+                toolbar: {
+                    buttons: ['bold', 'italic', 'underline', 'quote', 'anchor', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'orderedList', 'unorderedList', 'h2', 'h3'],
+                    static: true,
+                    sticky: true
+                },
+                placeholder: {
+                    text: 'Información de la vacante'
+                }
+            });
+
+            editor.subscribe('editableInput', function(eventObj, editable) {
+                const contenido = editor.getContent();
+                document.querySelector('#descripcion').value = contenido;
+            });
+        })
+    </script>
 @endsection
